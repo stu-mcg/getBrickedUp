344141d3c7cc0f497c5bcb023f788609349547b6
stu <stumcg@student.ubc.ca>
2022-11-20 22:39:18 -0800

    fixed bug with empty cart

--- code/routes/order.js
+++ code/routes/order.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res, next) {
     }
     if(!productList){
         res.write("<h2>There is nothing in the cart, <a href = 'listprod'>return to the shopping page</a></h2>")
+        res.end()
     }else{
         (async function() {
             try {
