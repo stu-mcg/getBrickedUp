f63ca1cc4b7037b05095a50456d50159b1f0bc29
mvibert1 <macvibert@gmail.com>
2022-12-07 20:29:53 -0800

    idk

--- code/routes/product.js
+++ code/routes/product.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
             let getProductInfo = "SELECT productId, productName, productPrice, productImageURL, productImage, productDesc FROM product WHERE productId = @productId"
             let info = await (await pool.request().input("productId", productId).query(getProductInfo)).recordset[0]
 
-            productName = () => info.productName.replace(/ /g, '%20'); 
+            productName = () => info.productName; 
             productId = () => info.productId;
             productPrice = () => info.productPrice;
             productDesc = () => info.productDesc;
@@ -58,38 +58,7 @@ router.get('/', function(req, res, next) {
                 productDesc: productDesc()
             });
         }
-        	// TODO: Retrieve and display info for the product
-
-            res.write(`<br><br><h2>Leave a review:</h2>`)
-            if(req.query.reviewFailedMessage){
-                res.write(`<h3>${req.query.reviewFailedMessage}</h3>`)
-            }
-            res.write(`
-            <form action="/addReview">
-            <input type="hidden" id="productId" name="productId" value="${productId}">
-                <label>Rating:</label>
-                <select id="rating" name="rating">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-                <p>Comment:</p>
-                <textarea id="comment" name="comment" rows="4" cols="50"></textarea>
-                <br>
-                <input type="submit" value="Submit">
-            </form>
-            `)
-
-            res.write(`<br><br><h2>Reviews:</h2>`)
-            let getReviews = `SELECT reviewRating, reviewDate, reviewComment, userid FROM review as R, customer AS C WHERE C.customerId = R.customerId and productId = @productId;`
-            reviews = (await pool.request().input("productId", productId).query(getReviews)).recordset
-            for(let i = 0; i < reviews.length; i++){
-                let review = reviews[i]
-                res.write(`<div style = "border:1px solid black"><h3>${review.userid} rated ${review.reviewRating}/5 on ${new Date(review.reviewDate).toDateString()}</h3><p>${review.reviewComment}</p><br></div>`)
-            }
-            res.end()
+       
         } catch(err) {
             console.dir(err);
             res.write(err + "")
