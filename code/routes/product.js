7a4945c6282f65abe61cd8c5229cbc98922dfd3c
mvibert1 <macvibert@gmail.com>
2022-12-07 22:22:30 -0800

    reviews on prod page

--- code/routes/product.js
+++ code/routes/product.js
@@ -7,39 +7,56 @@ router.get('/', function(req, res, next) {
     (async function() {
         try {
             let pool = await sql.connect(dbConfig);
+
             productId = req.query.id
             let getProductInfo = "SELECT productId, productName, productPrice, productImageURL, productImage, productDesc FROM product WHERE productId = @productId"
             let info = await (await pool.request().input("productId", productId).query(getProductInfo)).recordset[0]
-
             productName = () => info.productName; 
-            productId = () => info.productId;
+            productIdHbs = () => info.productId;
             productPrice = () => info.productPrice;
             productDesc = () => info.productDesc;
             prodNameURL = () => info.productName.replace(/ /g, '%20');
 
+            console.dir((await pool.request().query("Select * FROM review")).recordset)
+
+            let getReviews = `SELECT reviewRating, reviewDate, reviewComment, userid FROM review as R, customer AS C WHERE C.customerId = R.customerId and productId = @productId;`
+            reviews = (await pool.request().input("productId", productId).query(getReviews)).recordset
+            let reviewsHbs = () => [];
+            for(let i = 0; i < reviews.length; i++){
+                let review = reviews[i]
+                reviewsHbs[i] = {userid:`${review.userid}`,
+                                reviewRating:`${review.reviewRating}`,
+                                reviewComment: `${review.reviewComment}`,
+                                reviewDate: `${new Date(review.reviewDate).toDateString()}`
+                            }         
+            }
+        
+        
            if(info.productImageURL != null ){
                 images = () => `<img src = ${info.productImageURL}>`
                return res.render('prod',{layout: 'main', 
                 title: productName(), 
-                productId: productId(), 
+                productId: productIdHbs(), 
                 productName: productName(),
                 productPrice: productPrice(),
                 images: images(),
                 productDesc: productDesc(),
-                prodNameURL: prodNameURL()
+                prodNameURL: prodNameURL(),
+                reviews:reviewsHbs()
             });
             }
             else{
                 return res.render('prod',{layout: 'main', 
                 title: productName(), 
-                productId: productId(), 
+                productId: productIdHbs(), 
                 productName: productName(),
                 productPrice: productPrice(),
                 productDesc: productDesc(),
-                prodNameURL: prodNameURL()
+                prodNameURL: prodNameURL(),
+                reviews:reviewsHbs()
             });
         }
-       
+
         } catch(err) {
             console.dir(err);
             res.write(err + "")
